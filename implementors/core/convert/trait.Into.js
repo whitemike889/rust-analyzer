394dae711cab1ac1f0f1776202328f935e94db67
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-11 15:42:41 +0000

    Deploy rust-analyzer/rust-analyzer to github.com/rust-analyzer/rust-analyzer.git:gh-pages

--- implementors/core/convert/trait.Into.js
+++ implementors/core/convert/trait.Into.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["ra_hir"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"struct\" href=\"ra_hir/struct.Documentation.html\" title=\"struct ra_hir::Documentation\">Documentation</a>",synthetic:false,types:["ra_hir::docs::Documentation"]},];
+implementors["ra_hir"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"struct\" href=\"ra_hir/struct.Documentation.html\" title=\"struct ra_hir::Documentation\">Documentation</a>",synthetic:false,types:["ra_hir::code_model::docs::Documentation"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
