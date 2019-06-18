6d9f3d25b7e7069a1235fdbf6dd9d4706dd590b6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-18 14:58:49 +0000

    Deploy rust-analyzer/rust-analyzer to github.com/rust-analyzer/rust-analyzer.git:gh-pages

--- implementors/salsa/trait.Database.js
+++ implementors/salsa/trait.Database.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
-implementors["ra_batch"] = [{text:"impl Database for <a class=\"struct\" href=\"ra_batch/struct.BatchDatabase.html\" title=\"struct ra_batch::BatchDatabase\">BatchDatabase</a>",synthetic:false,types:["ra_batch::BatchDatabase"]},];
 implementors["ra_hir"] = [{text:"impl Database for <a class=\"struct\" href=\"ra_hir/mock/struct.MockDatabase.html\" title=\"struct ra_hir::mock::MockDatabase\">MockDatabase</a>",synthetic:false,types:["ra_hir::mock::MockDatabase"]},];
 
             if (window.register_implementors) {
