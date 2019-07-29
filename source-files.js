64289f6a532f13d4bede1377efe1ab13adb70e3c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-29 21:37:49 +0000

    Deploy rust-analyzer/rust-analyzer to github.com/rust-analyzer/rust-analyzer.git:gh-pages

--- source-files.js
+++ source-files.js
@@ -2,7 +2,7 @@ var N = null;var sourcesIndex = {};
 sourcesIndex["gen_lsp_server"] = {"name":"","files":["lib.rs","msg.rs","stdio.rs"]};
 sourcesIndex["pre_commit"] = {"name":"","files":["pre-commit.rs"]};
 sourcesIndex["ra_arena"] = {"name":"","files":["lib.rs","map.rs"]};
-sourcesIndex["ra_assists"] = {"name":"","files":["add_derive.rs","add_explicit_type.rs","add_impl.rs","add_missing_impl_members.rs","assist_ctx.rs","ast_editor.rs","auto_import.rs","change_visibility.rs","fill_match_arms.rs","flip_binexpr.rs","flip_comma.rs","inline_local_variable.rs","introduce_variable.rs","lib.rs","marks.rs","move_guard.rs","remove_dbg.rs","replace_if_let_with_match.rs","split_import.rs"]};
+sourcesIndex["ra_assists"] = {"name":"","files":["add_derive.rs","add_explicit_type.rs","add_impl.rs","add_missing_impl_members.rs","assist_ctx.rs","ast_editor.rs","auto_import.rs","change_visibility.rs","fill_match_arms.rs","flip_binexpr.rs","flip_comma.rs","inline_local_variable.rs","introduce_variable.rs","lib.rs","marks.rs","merge_match_arms.rs","move_guard.rs","remove_dbg.rs","replace_if_let_with_match.rs","split_import.rs"]};
 sourcesIndex["ra_batch"] = {"name":"","files":["lib.rs","vfs_filter.rs"]};
 sourcesIndex["ra_cli"] = {"name":"","files":["analysis_bench.rs","analysis_stats.rs","main.rs"]};
 sourcesIndex["ra_db"] = {"name":"","files":["cancellation.rs","input.rs","lib.rs"]};
